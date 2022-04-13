39b8cd7b74b056de0b5aac770a900aaf1994c6b4
Giuseppe <88315256+JIMMI74@users.noreply.github.com>
2022-04-13 10:12:26 +0200

    Update views.py
    add function sell to exchange

--- coinmarketalgo/views.py
+++ coinmarketalgo/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
 from accounts.coinmarketcap import algoValue
-from .forms import PurchaseForm
+from .forms import PurchaseForm, SaleCoin
 from django.contrib import messages
 from accounts.models import Profile
 from django.http import HttpResponseBadRequest
-from .models import PrincipalHome
+from .models import PrincipalHome, SellCoinExchange
 
 
 
@@ -43,6 +43,55 @@ def buyalgomkt(request):
     return render(
         request, "coinmarketalgo/purchase.html", {"form": form, "new_price": new_price}
     )
+
+
+
+
+
+def sellcoinexchange(request):
+    current_price_market = algoValue()
+    if request.method == "POST":
+        form = SaleCoin(request.POST)
+        if form.is_valid():
+            form.save(commit=False)
+            form.instance.profile = Profile.objects.get(user=request.user)
+            form.instance.sell_price = current_price_market
+            if (
+                form.instance.profile.ALGO_Wallet > 0
+                and form.instance.n_coin_sell <= form.instance.profile.ALGO_Wallet
+            ):
+                if form.instance.n_coin_sell > 0:
+                    form.instance.sale_coin = form.instance.n_coin_sell * current_price_market
+                    form.instance.commission_exchange = form.instance.sale_coin * form.instance.comm_exchange
+                   
+                    seller = Profile.objects.get(user=request.user)
+                    seller.ALGO_Wallet -= form.instance.n_coin_sell
+                    seller.USD_wallet += form.instance.sale_coin - form.instance.commission_exchange
+                    print(str(form.instance.commission_exchange) + "+" + str(seller.USD_wallet))
+                    form.save()
+                    seller.save()
+                    messages.success(request,
+                                     f"Your sales order has been sent to the exchange and processed,"
+                                     f" sales value = {seller.USD_wallet} USD, net of commission. ")
+
+                else:
+                    messages.warning(request, "Attention enter a correct value please! ")
+                return redirect("sell_exchange")
+
+            else:
+                messages.warning(request, "You do not have enough coins!")
+            return redirect("sell_exchange")
+
+        else:
+            return HttpResponseBadRequest()
+
+    else:
+        form = SaleCoin()
+
+    return render(
+        request, "coinmarketalgo/sell_exchange.html", {"form": form, "current_price_market": current_price_market}
+    )
+
 
 
 

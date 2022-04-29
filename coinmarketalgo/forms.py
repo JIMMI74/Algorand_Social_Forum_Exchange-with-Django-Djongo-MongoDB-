909013e7547894ed9a7e67699ef7d0fde2ad6c07
Giuseppe <88315256+JIMMI74@users.noreply.github.com>
2022-04-29 15:02:27 +0200

    add forms choices

--- coinmarketalgo/forms.py
+++ coinmarketalgo/forms.py
@@ -9,17 +9,23 @@ class PurchaseForm(forms.ModelForm):
 
     class Meta:
         model = Purchase
-        fields = ['max_spend_usd']
-        labels = {"max_spend_usd": "Check your USD availability and enter the number of coins you wish to purchase"}
-        
-        
-        
+        fields = ['max_spend_usd', 'purchased_price', 'positions']
+        labels = {"max_spend_usd": "Check your USD availability and enter the number of coins you wish to purchase",
+                  'purchased_price': 'Enter the PRICE',
+                  'positions': 'CURRENT PRICE/LIMIT PRICE'}
+        widgets = {
+            "purchased_price": forms.TextInput(attrs={"placeholder": "Enter the Price"})
+        }
+
+
 class SaleCoin(forms.ModelForm):
     current_price_market = algoValue()
 
     class Meta:
         model = SellCoinExchange
-        fields = ['n_coin_sell']
-        labels = {"n_coin_sell": "Enter the number of coins you wish to sell"}
-
-
+        fields = ['n_coin_sell', 'sell_price', 'positions']
+        labels = {"n_coin_sell": "Enter the number of coins you wish to sell", 'sell_price': 'Enter the PRICE',
+                  'positions': 'MARKET PRICE/LIMIT PRICE'}
+        widgets = {
+            "n_coin_sell": forms.TextInput(attrs={"placeholder": "Enter the number of coins you want to sell"})
+        }
